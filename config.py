39af9efd52ef787ffdd6f4f716275bec889cb206
mcity42 <47469354+mcity42@users.noreply.github.com>
2022-04-12 16:57:53 -0500

    added new files for api key and new api added

---
+++ config.py
@@ -0,0 +1,3 @@
+headers = {
+    'x-api-key': 'u5HRNDtcnX9CNvhqrHxqR32kjIPxBdPT2wfIdCrA'
+}
--- stocks_sentiment.py
+++ stocks_sentiment.py
@@ -1,14 +1,31 @@
 import requests
 import pprint
+import config as con
 
-curl = 'https://tradestie.com/api/v1/apps/reddit'
+wallst_curl = 'https://tradestie.com/api/v1/apps/reddit'
 
 # dictionary of the json response
-resp = requests.get(curl).json()
+resp = requests.get(wallst_curl).json()
 
-
+# display the sentiment for 50 prominent tickers
 for ticker in resp:
     pprint.pprint(ticker['ticker'] + ":" + ticker['sentiment'])
 
 
-# pprint.pprint(resp)
+print("------------------------------------------")
+
+yahoo_curl = 'https://yfapi.net/v6/finance/quote'
+
+
+queryInput = input(
+    "Enter 1 or more stock symbols separated by a comma,\n20 max.")
+
+query_string = {"symbols": f"{queryInput}"}
+
+querytest = {'symbols': 'AAPL'}
+
+
+responder = requests.request(
+    "Get", yahoo_curl, headers=con.headers, params=query_string).json()
+
+pprint.pprint(responder)
