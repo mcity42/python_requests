60955c21739d6f35b1d07958e9dbf7ebb67c7694
mcity42 <47469354+mcity42@users.noreply.github.com>
2022-04-12 17:09:52 -0500

    fix config

--- stocks_sentiment.py
+++ stocks_sentiment.py
@@ -9,24 +9,25 @@ resp = requests.get(wallst_curl).json()
 
 # display the sentiment for 50 prominent tickers
 for ticker in resp:
-    pprint.pprint(ticker['ticker'] + ":" + ticker['sentiment'])
+    pprint.pprint(ticker['ticker'] + ": " + ticker['sentiment'])
 
 
 print("------------------------------------------")
 
 yahoo_curl = 'https://yfapi.net/v6/finance/quote'
 
-
+print("Choose ticker(s) above to analyze further")
 queryInput = input(
-    "Enter 1 or more stock symbols separated by a comma,\n20 max.")
+    "Enter 1 or more tikers listed above separated by a comma,20 max: \n")
 
+# place user input into query
 query_string = {"symbols": f"{queryInput}"}
 
-querytest = {'symbols': 'AAPL'}
 
 # responder builds request with user input as query params
 # it takes an api key registered for yahoo finance api and saved
-# in the config.py file
+# in the config.py file saved as
+# headers = {'x-api-key':'YOUR-PERSONAL-API_KEY'}
 responder = requests.request(
     "Get", yahoo_curl, headers=con.headers, params=query_string).json()
 
