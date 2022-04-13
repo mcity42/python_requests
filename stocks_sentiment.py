06744b7e999af50f95f8002de88caab8dcd082e6
mcity42 <47469354+mcity42@users.noreply.github.com>
2022-04-12 23:49:10 -0500

    maybe gui

--- stocks_sentiment.py
+++ stocks_sentiment.py
@@ -1,6 +1,7 @@
 import requests
 import pprint
 import config as con
+from tkinter import *
 
 wallst_curl = 'https://tradestie.com/api/v1/apps/reddit'
 
@@ -18,7 +19,7 @@ yahoo_curl = 'https://yfapi.net/v6/finance/quote'
 
 print("Choose ticker(s) above to analyze further")
 queryInput = input(
-    "Enter 1 or more tikers listed above separated by a comma,20 max: \n")
+    "Enter 1 or more tikers listed above separated by a comma,20 max: \n").upper()
 
 # place user input into query
 query_string = {"symbols": f"{queryInput}"}
@@ -33,3 +34,29 @@ responder = requests.request(
 
 # pretty print
 pprint.pprint(responder)
+
+
+# class Window(Frame):
+#     def __init__(self, master=None):
+#         Frame.__init__(self, master)
+#         self.master = master
+
+#         self.pack(fill=BOTH, expand=1)
+
+#         exitButton = Button(self, text="Exit", command=self.clickExitButton)
+
+#         exitButton.place(x=0, y=0)
+
+#     def clickExitButton(self):
+#         exit()
+
+
+# root = Tk()
+# app = Window(root)
+# root.wm_title("Stocks")
+# root.geometry('320x200')
+# root.mainloop()
+
+# stats = Text(root)
+# stats.insert(INSERT, resp)
+# stats.pack()
