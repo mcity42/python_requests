1d30ab9b7d64802d8e8767aa61194460c80a93a3
mcity42 <47469354+mcity42@users.noreply.github.com>
2022-04-14 11:55:52 -0500

    done

--- stocks_sentiment.py
+++ stocks_sentiment.py
@@ -1,69 +1,54 @@
 #!/usr/bin/python3
 
-from locale import currency
-from flask import render_template, url_for
-from flask import request
-from flask import redirect
-from flask import Flask
-from flask import session
-from random import randint
+# Imports if Flask endpoints are incorporated to application
+# from flask import render_template, url_for
+# from flask import request
+# from flask import redirect
+# from flask import Flask
 import requests
 import pprint
 import config as con
 import random
-
-
-#import pandas as pd
 
 wallst_curl = 'https://tradestie.com/api/v1/apps/reddit'
 
 # dictionary of the json response
 resp = requests.get(wallst_curl).json()
 
-ticker_list = []
+#ticker_list = []
 
 # display the sentiment for 50 prominent tickers
 i = 1
 for ticker in resp:
     pprint.pprint(f"{i}. " + ticker['ticker'] + ": " + ticker['sentiment'])
-    ticker_list += ticker['ticker'] + ": " + ticker['sentiment']
+    #ticker_list = ticker_list.append(ticker['ticker'] + ": " + ticker['sentiment'])
     i += 1
 
-# pprint.pprint(pd.Series(ticker_list))
 
 print("------------------------------------------")
 
 yahoo_curl = 'https://yfapi.net/v6/finance/quote'
-
-
-print("Choose ticker(s) above to analyze further")
 queryInput = ''
-while queryInput == '':
-    queryInput = input(
-        "Enter 1 ticker from the above list: \n").upper()
+
+
+def searchByInput():
+    print("Choose ticker(s) above to analyze further")
+    while queryInput == '':
+        queryInput = input("Enter 1 ticker from the above list: \n").upper()
+
+
 print('\n')
-# chart
-# chart_url = f'https://yfapi.net/v6/finance/chart'
-# chart_string = {"ticker": f"{queryInput}"}
-# chart = requests.request(
-#     "Get", chart_url, headers=con.headers, params=chart_string).json()
-# pprint.pprint(chart)
-
-# place user input into query
 query_string = {"symbols": f"{queryInput}"}
-
 
 # responder builds request with user input as query params
 # it takes an api key registered for yahoo finance api and saved
 # in the config.py file saved as
 # headers = {'x-api-key':'YOUR-PERSONAL-API_KEY'}
-
 responder = requests.request(
     "Get", yahoo_curl, headers=con.headers, params=query_string).json()
 
-
 # pprint.pprint(responder)
-# pretty print
+# pretty print all the wanted key-values from response
 print("Symbol: ", end='')
 symbol = pprint.pprint(responder['quoteResponse']['result'][0]['symbol'])
 print("Name: ", end='')
@@ -120,43 +105,44 @@ pprint.pprint(insight['finance']['result']['reports'][rand_insight]['summary'])
 print("Date: ", end='')
 pprint.pprint(insight['finance']['result']['reports'][0]['publishedOn'])
 
-print("------------------------------------------")
+print("-------------------------------------------------")
+searchByInput()
 
+
+# Future additions: add an endpoint and allow search/request from form input submit
 # -------------------------------------------------------
-user_symbol = ''
+# user_symbol = ''
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def start():
-    return render_template("postmaker.html")
+# app = Flask(__name__)
 
 
-@app.route("/login", methods=["POST"])
-def login():
-    if request.form.get("nm"):
-        user_symbol = request.form.get("nm")
-        if user_symbol != '':
-            url = 'https://yfapi.net/v6/finance/quote'
-            query2 = {"symbols": f"{user_symbol}"}
-            res = requests.request(
-                "Get", url, headers=con.headers, params=query2).json()
-            session['res'] = res
-            session.modified = True
-            return redirect("/correct")
-        else:
-            return redirect("/")
-    else:
-        return redirect("/")
+# @app.route("/")
+# def start():
+#     return render_template("postmaker.html")
+#     # return f"Symbol: {symbol}\nName: {name}\n52 Week Range: {yearrange}\nMarket Cap: {cap}\nCurrency: {currencytype}\nAsk: {ask}\nBid: {bid}\nSource: {source}"
 
 
-@app.route("/correct")
-def success():
-    res = request.args['res']
-    query_symbol = pprint.pprint(res['quoteResponse']['result'])
-    return f"Symbol: {query_symbol}\nName: {name}\n52 Week Range: {yearrange}\nMarket Cap: {cap}\nCurrency: {currencytype}\nAsk: {ask}\nBid: {bid}\nSource: {source}"
+# @app.route("/login", methods=["POST"])
+# def login():
+#     if request.form.get("nm"):
+#         user_symbol = request.form.get("nm")
+#         if user_symbol != '':
+#             return redirect(url_for("success", ticker=user_symbol))
+#         else:
+#             return redirect("/")
+#     else:
+#         return redirect("/")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=2224)  # runs the application
+# @app.route("/success/<ticker>", methods=["GET", "POST"])
+# def success(ticker):
+#     url = 'https://yfapi.net/v6/finance/quote'
+#     query2 = {"symbols": f"{ticker}"}
+#     res = requests.request(
+#         "Get", url, headers=con.headers, params=query2).json()
+#     query_symbol = pprint.pprint(res['quoteResponse']['result'])
+#     return f"Symbol: {query_symbol}\nName: {name}\n52 Week Range: {yearrange}\nMarket Cap: {cap}\nCurrency: {currencytype}\nAsk: {ask}\nBid: {bid}\nSource: {source}"
+
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=2224)  # runs the application
